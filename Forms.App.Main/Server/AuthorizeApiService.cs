60c9d0dbc11760920af2ea58a5e69abd3f18cc45
Lycoris <zzyo.yj@outlook.com>
2025-07-04 20:51:08 +0800

    移除测试用例

--- Forms.App.Main/Server/AuthorizeApiService.cs
+++ Forms.App.Main/Server/AuthorizeApiService.cs
@@ -12,43 +12,5 @@ namespace Forms.App.Main.Server
     public class AuthorizeApiService : BaseApiService
     {
         public AuthorizeApiService(IServiceProvider serviceProvider) : base(serviceProvider) { }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public void Test()
-        {
-            this.Logger.Info("1");
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="a"></param>
-        /// <returns></returns>
-        [ApiMethod("TestReturn")]
-        public TestRe Test(string a)
-        {
-            this.Logger.Info(a);
-            return  new TestRe();
-        }
-    }
-
-    public class TestRe()
-    {
-        public string Name { get; set; } = "123";
-
-        private string ddd { get; set; }
-
-        public TestReCC CC { get; set; } = new TestReCC();
-    }
-
-    public class TestReCC()
-    {
-        public string Name { get; set; } = "123";
-
-        private string ddd { get; set; }
-
-        public string CC { get; set; }
     }
 }
